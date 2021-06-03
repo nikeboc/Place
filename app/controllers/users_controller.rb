6187cafa7b86fb96d03c93f774c6938268deea47
nikeboc <nikebocgtr@gmail.com>
2021-06-03 17:51:13 +0900

    Finish edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -13,9 +15,24 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Placeへようこそ!"
+      # ホーム画面に飛ばした方が良いかも
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "ログイン情報を更新しました！"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -24,5 +41,22 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # before
+
+    # ログインの確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+
+    # ログインユーザーが同一かどうか
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
