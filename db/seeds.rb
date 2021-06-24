be9fba63b79dd094215e0760bb6105b29148301b
nikeboc <nikebocgtr@gmail.com>
2021-06-24 12:47:41 +0900

    chnaged layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Sample",
+    email: "sample@example.com",
+    password:              "samplepass",
+    password_confirmation: "samplepass",
+    id: 1
+    )
+
+User.create!(name:  "hoge",
+    email: "hoge@example.com",
+    password:              "samplepass",
+    password_confirmation: "samplepass",
+    id: 2
+    )
+
+
+# Post.create!(content: "都心から約１時間の癒しの里山「旧藤野町（現相模原市緑区）」に農業法人藤野倶楽部があります。
+#     自社で野菜やお茶を作る他、その野菜を生かした直営の農園レストラン「百笑の台所」や、オーガニックの自然食品コーナー、
+#     会員制の貸し農園「安心農園」、採れたて野菜がそのまま焼ける「百笑バーベキュー場」「テニスコート」「貸し古民家」など、
+#     農的な暮らしがゆったりと楽しめる場所になっています。また、提携の源泉かけ流しの天然温泉も近くにあります。眼下に広
+#     がる景色はこのエリアでも見晴らしの良い場所です。バーベキュー施設も隣接しており、のんびりと過ごす事が出来ます。テ
+#     ニスコート、レストラン。近くには温泉もあるので緑の中でいろいろ楽しめます。 キャンピングカーにはベットが4台。ちょ
+#     っと古いタイプなので、その点はご了承ください。備え付けのクーラーはありますが、夏場は少し暑いかと思われます。冬は
+#     オイルヒーターがありますが、少し寒いです。小さい冷蔵庫はついており、またBBQ場での食器、調理器具などがお使い頂け
+#     ます。ガス台利用でしたら無料、炭の利用には880円（税込）かかります。",
+#    title: "相模原キャンプ場",
+#    price: 5000,
+#    location: "神奈川県相模原市",
+#    image: File.open("/app/assets/images/sagamihara.jpg"),
+#    id: 1
+#    )
+
+
